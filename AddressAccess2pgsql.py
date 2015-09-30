188863df3b45e38bc5ae20f994a9797e98432e1a
Martin Høgh <mh@mapcentia.com>
2015-09-30 12:57:11 +0200

    AWS 4

--- AddressAccess2pgsql.py
+++ AddressAccess2pgsql.py
@@ -5,51 +5,67 @@ filesource = args[1]
 sql = "DROP TABlE " + args[2] + ";"+\
 		"BEGIN;" +\
 		"CREATE TABLE " + args[2] + " (" +\
-		"gid SERIAL PRIMARY KEY," +\
-		"AddressAccessIdentifier varchar," +\
-		"VersionId varchar," +\
-		"BuildingName varchar," +\
-		"MunicipalityCode varchar," +\
-		"StreetCode varchar," +\
-		"StreetName varchar," +\
-		"StreetBuildingIdentifier varchar," +\
-		"DistrictSubdivisionIdentifier varchar," +\
-		"PostCodeIsdentifier varchar," +\
-		"DistrictName varchar," +\
-		"CadastralDistrictIdentifier varchar," +\
-		"CadastralDistrictName varchar," +\
-		"LandParcelIdentifier varchar," +\
-		"MunicipalRealPropertyIdentifier varchar," +\
-		"the_geom geometry(Point,25832)" +\
+		"id varchar," +\
+		"status int," +\
+		"oprettet varchar," +\
+		"aendret varchar," +\
+		"vejkode varchar," +\
+		"vejnavn varchar," +\
+		"adresseringsvejnavn varchar," +\
+		"husnr varchar," +\
+		"supplerendebynavn varchar," +\
+		"postnr varchar," +\
+		"postnrnavn varchar," +\
+		"stormodtagerpostnr varchar," +\
+		"stormodtagerpostnrnavn varchar," +\
+		"kommunekode varchar," +\
+		"kommunenavn varchar," +\
+		"ejerlavkode varchar," +\
+		"ejerlavnavn varchar," +\
+		"matrikelnr varchar," +\
+		"esrejendomsnr varchar," +\
+		"etrs89koordinat_oest varchar," +\
+		"etrs89koordinat_nord varchar," +\
+		"wgs84koordinat_bredde varchar," +\
+		"wgs84koordinat_laengde varchar," +\
+		"noejagtighed varchar," +\
+		"kilde varchar," +\
+		"tekniskstandard varchar," +\
+		"tekstretning varchar," +\
+		"adressepunktaendringsdato varchar," +\
+		"ddkn_m100 varchar," +\
+		"ddkn_km1 varchar," +\
+		"ddkn_km10 varchar," +\
+		"regionskode varchar," +\
+		"regionsnavn varchar," +\
+		"jordstykke_ejerlavnavn varchar," +\
+		"kvh varchar," +\
+		"sognekode varchar," +\
+		"sognenavn varchar," +\
+		"politikredskode varchar," +\
+		"politikredsnavn varchar," +\
+		"retskredskode varchar," +\
+		"retskredsnavn varchar," +\
+		"opstillingskredskode varchar," +\
+		"opstillingskredsnavn varchar," +\
+		"zone varchar," +\
+		"jordstykke_ejerlavkode varchar," +\
+		"jordstykke_matrikelnr varchar," +\
+		"jordstykke_esrejendomsnr varchar," +\
+		"the_geom geometry(Point,4326)," +\
+		"gid SERIAL PRIMARY KEY" +\
 		");" +\
 		"COMMIT;"
 print sql
 #sys.exit()
 #print "BEGIN;"
 with open(filesource, 'rb') as f:
-    reader = csv.reader(f, delimiter=';', quotechar='"')
+    reader = csv.reader(f, delimiter=',', quotechar='"')
     i = 1
     for row in reader:
 	if i>1:
 		statement = "INSERT INTO " + args[2] + " " + \
-				    "(" +\
-				    "AddressAccessIdentifier," + \
-				    "VersionId," + \
-				    "BuildingName," +\
-				    "MunicipalityCode," +\
-				    "StreetCode," +\
-				    "StreetName," +\
-				    "StreetBuildingIdentifier," +\
-				    "DistrictSubdivisionIdentifier," +\
-				    "PostCodeIsdentifier," +\
-				    "DistrictName," +\
-				    "CadastralDistrictIdentifier," +\
-				    "CadastralDistrictName," +\
-				    "LandParcelIdentifier," +\
-				    "MunicipalRealPropertyIdentifier," +\
-				    "the_geom" +\
-				    ") " + \
-				    "VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',ST_geomfromtext('POINT(%s %s)',25832));" % (tuple([w.replace("'","''") for w in row[0:14]]) + tuple(row[17:19]))
+				    "VALUES ('%s',%s,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',ST_geomfromtext('POINT(%s %s)',4326));" % (tuple([w.replace("'","''") for w in row[0:47]]) + (row[22],row[21]))
 		print statement
 	i = i + 1
 #print "COMMIT"
